b7483b58f905360c14bce038e39329d451410c9b
Dulio Denis <dulio.denis@yahoo.com>
2018-02-05 11:42:24 -0500

    Enabled Camera Functionality

--- Hotdog/Hotdog/ViewController.swift
+++ Hotdog/Hotdog/ViewController.swift
@@ -7,8 +7,37 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary //.camera
+        imagePicker.allowsEditing = false
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+
 }
 
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        
+        imagePicker.dismiss(animated: true)
+    }
+    
+}
